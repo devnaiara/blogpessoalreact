95f5c6b83db7c191f041451fa47d3e975abddbc9
devnaiara <devnaiara@outlook.com>
2025-01-16 22:11:17 -0300

    add service e model

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Navbar from './assets/components/navbar/Navbar';
 import Footer from './assets/components/footer/Footer';
 import Home from './pages/home/Home';
+import Cadastro from './pages/cadastro/Cadastro';
+import Login from './pages/login/Login';
+
 // import Postagens from './pages/Postagens';
 // import Temas from './pages/Temas';
 // import CadastrarTemas from './pages/CadastrarTemas';
@@ -13,15 +16,19 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        {/* <Route path="/postagens" element={<Postagens />} />
-        <Route path="/temas" element={<Temas />} />
-        <Route path="/cadastrar-temas" element={<CadastrarTemas />} />
-        <Route path="/perfil" element={<Perfil />} />
-        <Route path="/sair" element={<Sair />} /> */}
-      </Routes>
+      <div className="min-h-[80vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/postagens" element={<Postagens />} />
+          <Route path="/temas" element={<Temas />} />
+          <Route path="/cadastrar-temas" element={<CadastrarTemas />} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/sair" element={<Sair />} /> */}
+        </Routes>
+      </div>
       <Footer />
     </BrowserRouter>
   );
--- src/assets/components/navbar/Navbar.tsx
+++ src/assets/components/navbar/Navbar.tsx
@@ -6,13 +6,13 @@ function Navbar () {
                         bg-indigo-900 text-white">
 
             <div className="container flex justify-between text-lg">
-                <Link to="/home"> Blog Pessoal </Link>
+                <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
                 <div className="flex space gap-4">
                     <Link to="/" className="text-white">Home</Link>
                     <Link to="/Postagens" className="text-white">Postagens</Link>
                     <Link to="/Temas" className="text-white">Tema</Link>
-                    <Link to="/Cadastrar Temas" className="text-white">Cadastrar Tema</Link>
+                    <Link to="/Cadastro" className="text-white">Cadastrar</Link>
                     <Link to="/Perfil" className="text-white">Perfil</Link>
                     <Link to="/Sair" className="text-white">Sair</Link>
                 </div>
